1ffd1dce9a0034b5ce0d4d8e69578179d5b6f91c
TetoBertolla1 <42170640+TetoBertolla1@users.noreply.github.com>
2020-01-31 18:42:50 -0300

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -383,13 +383,8 @@ namespace CryptoNote
        { 0xB1, 0xB3, 0xB5, 0xB7, 0xB9, 0x5i, 0x7i, 0x9i, 0xcu, 0xcs, 0xc5, 0xc7, 0xc9, 0xc3, 0xc1, 0xD2 }};
 
     const char *const SEED_NODES[] = {
-        "206.189.142.142:11897", // rock
-        "145.239.88.119:11999", // cision
-        "142.44.242.106:11897", // tom
-        "165.227.252.132:11897", // iburnmycd
-        "148.251.178.238:11897", // hv
-        "45.32.138.7:11897", // extra
-        "46.214.70.196:11897", // CuveeRO
-        "94.113.119.122:11897" // CuveeCZ
+        "13.92.63.45:11899", // rock
+        "145.239.88.119:11991", // cision
+     
     };
 } // namespace CryptoNote
